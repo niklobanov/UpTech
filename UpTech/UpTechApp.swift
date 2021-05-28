d0112fa620a545fcd57b53fadbf7b1ece7aa486a
Nikita Lobanov <nlobanov@sberhealth.ru>
2021-05-28 19:14:02 +0300

    Initial Commit

---
+++ UpTech/UpTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpTechApp.swift
+//  UpTech
+//
+//  Created by Nikita Lobanov on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct UpTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
